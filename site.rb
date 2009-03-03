e4d737130c7581b746a19ac89863dbfb9fd05127
Burke Libbey <burke@burkelibbey.org>
2009-03-02 23:51:03 -0600

    layout changes

--- site.rb
+++ site.rb
@@ -4,10 +4,25 @@ require 'haml'
 require 'compass'
 
 class Site < Sinatra::Base
-  get '/?' do
-    haml :index
+
+  helpers do
+    def link_to(text,url)
+      "<a href=\"#{url}\">#{text}</a>"
+    end
   end
 
+  get '/?' do
+    haml :index, :locals => {:body_class => 'home'}
+  end
+
+  get '/work/?' do
+    haml :work
+  end
+  
+  get '/secret/?' do
+    haml :secret, :locals => { :blurb => 'You win the internet!' }
+  end
+  
   not_found do
     haml :not_found
   end
